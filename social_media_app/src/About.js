0553c2fc50231b10a2cdee270008187c66c243d7
Ramkumar V <ramkumarvenkatesan02@gmail.com>
2026-04-08 17:01:30 +0530

    Social_media_app

---
+++ social_media_app/src/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <main className='About'>
+            <h2>About</h2>
+            <p style={{ marginTop: "1rem" }}>This is Ramkumar React Project Build With React </p>
+        </main>
+    )
+}
+
+export default About
